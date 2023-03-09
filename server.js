d18ee9dafb86b6a4af6a3d1801d7217ebae25fe4
Krrish <malekm09122003@gmail.com>
2023-03-09 19:58:56 +0530

    Adding commonly used Middlewares

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const {logger} = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 const PORT = process.env.PORT || 3500
 
-app.use('/', express.static(path.join(__dirname,'/public')))
+app.use(logger)
+app.use(express.json())
+app.use(express.static('public'))
+app.use(cookieParser())
+app.use(cors(corsOptions))
 
 app.use('/', require('./routes/root'))
 
@@ -16,6 +25,8 @@ app.use('*', (req,res) => {
     else 
         res.type('txt').send('404 Not Found!')
 })
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}!`))
 
